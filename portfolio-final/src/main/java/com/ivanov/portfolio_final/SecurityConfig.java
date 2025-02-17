78425f875f37b15ae183549b95d1c849bf8f7b04
hivanov11 <hristogr85@gmail.com>
2025-02-17 00:08:09 -0500

    Fixes.

--- portfolio-final/src/main/java/com/ivanov/portfolio_final/SecurityConfig.java
+++ portfolio-final/src/main/java/com/ivanov/portfolio_final/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Allow frontend
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://portfolio-app-frontend-dtrod.ondigitalocean.app")); // Allow frontend
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         config.setAllowCredentials(true); // Allow credentials
